c8ed8f3d9fdbdd265f6ce54b834eec57cd42b10b
Barak Saidoff <barak.saidoff@gmail.com>
2020-03-19 14:26:54 -0400

    update nav footer hover

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
         <div className="footer">
             <h3 style={{color: "#fff", fontSize: 21, paddingTop: 54, paddingBottom: 8, fontWeight: "700", marginTop: 42.5}}>React JavaScript Frontend</h3>
             <h5 style={{color: "#fff", fontWeight: "300"}}>Ruby on Rails Backend</h5>
-            <button className= "buttonF" style={{backgroundColor: "#F8981C", color: "#fff", width: 145, padding: 8.5, fontWeight: "700", fontSize:15, borderRadius: 40, borderColor: "#F8981c", marginTop: 26,}}>Barak's Portfolio</button>
+            <button className= "buttonF" style={{backgroundColor: "#E6EFF6", color: "#013C71", width: 145, padding: 8.5, fontWeight: "700", fontSize:15, borderRadius: 40, borderColor: "#E6EFF6", marginTop: 26,}}>Barak's Portfolio</button>
         </div>
     )
 }
